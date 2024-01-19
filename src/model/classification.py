27048b9fea2b296e0e87dade83bbe32a7e644b34
Nicolas Dufour <nicolas.dufourn@gmail.com>
2024-01-19 18:49:21 +0100

    Debug hangs at end

--- src/model/classification.py
+++ src/model/classification.py
@@ -46,16 +46,16 @@ class ClassificationModule(L.LightningModule):
         self.train_metrics.update(pred, label)
         return loss
 
-    def on_train_epoch_end(self):
-        metrics = self.train_metrics.compute()
-        for metric_name, metric_value in metrics.items():
-            self.log(
-                f"train/{metric_name}",
-                metric_value,
-                sync_dist=True,
-                on_step=False,
-                on_epoch=True,
-            )
+    # def on_train_epoch_end(self):
+    #     metrics = self.train_metrics.compute()
+    #     for metric_name, metric_value in metrics.items():
+    #         self.log(
+    #             f"train/{metric_name}",
+    #             metric_value,
+    #             sync_dist=True,
+    #             on_step=False,
+    #             on_epoch=True,
+    #         )
 
     def validation_step(self, batch, batch_idx):
         img, label = batch
